a330aea8371bdda1ca4536070c010b7b2fe32ba9
anudev556 <anudev@tech-x.com>
2023-06-25 18:19:25 +0530

    fix: missing return

--- calc/calc.go
+++ calc/calc.go
@@ -1,19 +1,21 @@
 package calc
 
-import(
-  "fmt"
-"errors"
+import (
+	"errors"
+	"fmt"
 )
 
 func Add(args ...int) error {
 	if len(args) < 0 {
-return errors.New("minimum two arguments are required")
-} else {
-sum := 0
-for _, arg := range args {
-sum += arg
-}
-fmt.Println("Sum:", sum)
-}
+		return errors.New("minimum two arguments are required")
+	} else {
+		sum := 0
+		for _, arg := range args {
+			sum += arg
+		}
+		fmt.Println("Sum:", sum)
+	}
+
+	return nil
 
 }
