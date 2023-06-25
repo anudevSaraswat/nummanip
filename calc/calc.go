dea9e09b977207d914c8f421f9a0a858ac53c16c
anudev556 <anudev@tech-x.com>
2023-06-25 17:50:15 +0530

    Add function modified

--- calc/calc.go
+++ calc/calc.go
@@ -2,8 +2,18 @@ package calc
 
 import(
   "fmt"
+"errors"
 )
 
-func Add(a, b int) {
-   fmt.Println("Sum:", a+b)
+func Add(args ...int) error {
+	if len(args) < 0 {
+return errors.New("minimum two arguments are required")
+} else {
+sum := 0
+for _, arg := range args {
+sum += arg
+}
+fmt.Println("Sum:", sum)
+}
+
 }
